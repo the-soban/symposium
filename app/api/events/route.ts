57e2d5e1ec32080631416059e60bdbedaf804f17
Soban Shafiq <sobans@agencyva.com>
2026-07-08 00:06:50 +0500

    added basic api routing, needs review

---
+++ app/api/events/route.ts
@@ -0,0 +1,26 @@
+import Event from "@/database/event.model";
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectDB();
+
+        const formData = await req.formData();
+        let event;
+
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch (e) {
+            return NextResponse.json({ message: 'Invalid JSON data format' }, { status: 400 });
+        }
+
+        const createdEvent = await Event.create(event);
+
+        return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 });
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ message: 'Event creation failed', error: e instanceof Error ? e.message : 'Unknown' }, { status: 500 });
+        
+    }
+}
